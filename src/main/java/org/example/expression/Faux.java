6e787e5688aeb774149b210394701585d5195bd0
ToavinaMaria <ToavinaMaria>
2024-06-07 16:41:34 +0300

    feat: implements class Faux.java

---
+++ src/main/java/org/example/expression/Faux.java
@@ -0,0 +1,17 @@
+package org.example.expression;
+
+import java.util.Map;
+
+public final class Faux extends Expression {
+    private String valeurFaux;
+
+    public Faux(String valeurFaux) {
+        this.valeurFaux = valeurFaux;
+    }
+
+    @Override
+    public Boolean evalue(Map<String, Boolean> contexte) {
+        Boolean valeur = contexte.getOrDefault(valeurFaux,null);
+        return valeur != null && !valeur;
+    }
+}
