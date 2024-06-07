31f0be6bd952082b4d9b8bd5d8d981a51b44652f
ToavinaMaria <ToavinaMaria>
2024-06-07 16:44:22 +0300

    feat: implements class FauxTest.java

---
+++ src/test/java/FauxTest.java
@@ -0,0 +1,18 @@
+import org.example.expression.Faux;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FauxTest {
+    @Test
+    void testFaux() {
+        Faux faux = new Faux("Lou est pauvre");
+        Map<String, Boolean> contexte = new HashMap<>();
+        contexte.put("Lou est pauvre", false);
+        Boolean resultat = faux.evalue(contexte);
+        assertTrue(resultat);
+    }
+}
